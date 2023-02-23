8ab5bfd9b25edf27d9a8bb224900dbcdc8d85492
Austin Papritz <austin@papritz.dev>
2023-02-23 12:02:16 -0800

    basic SynthContext

---
+++ src/context/SynthContext.js
@@ -0,0 +1,20 @@
+import { useContext, useState } from 'react';
+import { createContext } from 'react';
+
+const SynthContext = createContext();
+
+const SynthProvider = ({ children }) => {
+  const { amp, setAmp } = useState([]);
+  return <SynthContext.Provider value={{ amp, setAmp }}>{children}</SynthContext.Provider>;
+};
+
+const useSynthContext = () => {
+  const context = useContext(SynthContext);
+
+  if (context === undefined) {
+    throw new Error('useSynthContext must be used within a SynthProvider');
+  }
+  return context;
+};
+
+export { SynthProvider, useSynthContext };
