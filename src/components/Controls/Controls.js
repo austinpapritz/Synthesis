1ad196dd795a3251c857e3d4c4e249d5bca27d90
Austin Papritz <austin@papritz.dev>
2023-02-23 16:18:43 -0800

    only using one Oscillator component for now

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -6,10 +6,6 @@ export default function Controls() {
   return (
     <div className="controls-container">
       <Oscillator oscNum="1" />
-      <Oscillator oscNum="2" />
-      <Oscillator oscNum="3" />
-      <Oscillator oscNum="4" />
-      <Oscillator oscNum="5" />
     </div>
   );
 }
